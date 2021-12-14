92f4428a4cbeea07aae0a8f5b3c4468fce8bdf38
rabbithay <thay.csb@gmail.com>
2021-12-14 12:05:42 -0300

    Initializing project

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import './setup';
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import routes from './routers/useRouter';
+import { serverMiddlewareError } from './error/serverMiddlewareErro';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(serverMiddlewareError);
+
+export default app;
